36c9266138248f12a2706b3919b39d7deb064390
umyt gopher <rubickerne@gmail.com>
2023-11-06 22:50:49 +0500

    some changes

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	todo "github.com/umyt-king/todo"
 )
 
 const (
-	todoFile = "todos.json"
+	todoFile = "../todo.json"
 )
 
 func main() {
 	add := flag.Bool("add", false, "add a new todo")
 	flag.Parse()
 
-	todo := &todo.Todos{}
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
